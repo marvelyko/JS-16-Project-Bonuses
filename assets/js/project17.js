4d4e0bd4f035571ce14249bcc5c6a9608620ead1
marvelyko <karlo.tevzadze.1@iliauni.edu.ge>
2022-10-15 14:10:23 +0400

    add project17

--- assets/js/project17.js
+++ assets/js/project17.js
@@ -93,14 +93,52 @@ let words = [
     },
 ]
 
-const scrambleWord = document.querySelector('.word');
-const hint = document.querySelector('.hint');
-const timeLeft = document.querySelector('.time');
-const enterValid = document.querySelector('.valid-word');
-const refreshWord = document.querySelector('.refresh-word');
-const checkWord = document.querySelector('.check-word');
+const wordText = document.querySelector('.word');
+const hintText = document.querySelector('.hint');
+const timeText = document.querySelector('.time');
+const inputField = document.querySelector('.valid-word');
+const refreshBtn = document.querySelector('.refresh-word');
+const checkBtn = document.querySelector('.check-word');
 const result = document.querySelector('.result');
 
-const initGame = () => {
+let correctWord, timer;
 
+const initTimer = (maxTime) => {
+    clearInterval(timer);
+    timer = setInterval(() => {
+        if(maxTime > 0){
+            maxTime--;
+            return timeText.innerText = maxTime;
+        }
+        alert(`Time off! ${correctWord.toUpperCase()} was the correct word`);
+        initGame();
+    }, 1000)
 }
+
+const initGame = () => {
+    initTimer(30);
+    let randomObj = words[Math.floor(Math.random() * words.length)];
+    let wordArray = randomObj.word.split('');
+    for (let i = wordArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i+1));
+        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+    }
+    wordText.innerText = wordArray.join('');
+    hintText.innerText = randomObj.hint;
+    correctWord = randomObj.word.toLowerCase();
+    inputField.value = '';
+    inputField.setAttribute('maxlength', correctWord.length);
+}
+
+const checkWord = () => {
+    let userWord = inputField.value.toLowerCase();
+    if(!userWord) return alert("Please enter the word to check!");
+    if(userWord !== correctWord) return alert (`Oops! ${userWord} is no correct`);
+    alert(`Congrats! ${correctWord.toUpperCase()} is the correct`);
+    initGame();
+}
+
+refreshBtn.addEventListener('click', initGame);
+checkBtn.addEventListener('click', checkWord);
+
+initGame();
